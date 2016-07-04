3f171825ed49df5ba2dea701500dc61f260efb7b
jiahao <monkey19911021@gmail.com>
2016-07-04 16:06:08 +0800

    添加下标

--- TEstSwift/Subscripts.swift
+++ TEstSwift/Subscripts.swift
@@ -10,4 +10,43 @@ import Foundation
 
 func subscripts(){
     
+    var timesTable = TimesTabel()
+    timesTable.multipler = 5
+    print("整型下标：\(timesTable[3])")
+    print("字符串下标：\(timesTable["hello"])")
+    
+    timesTable[3] = 90
+    print("为带下标实例赋值后：\(timesTable.multipler)")
+}
+
+struct TimesTabel{
+    var multipler: Int = 0
+    
+    //可以定义多个下标，通过不同的参数类型进行重载
+    //下标可以设定为读写或只读，跟计算属性一样
+    subscript(index: Int) -> Int{
+        get{
+            return multipler * index
+        }
+        set{
+            
+            multipler = newValue/index
+            
+        }
+    }
+    
+    //只返回 getter 就是只读下标
+    subscript(str: String) -> String{
+        var result = ""
+        for _ in 0 ... multipler{
+            result += str + ", "
+        }
+        return result;
+    }
+}
+
+struct Matrix{
+    let rows: Int, columns: Int
+    var grid: [Double]
+    
 }
